42b1f6790bf5b062e571ce3175ce4641a19eb13c
ArcPer <arytvart63@gmail.com>
2022-02-18 12:04:04 +0300

    1

---
+++ Source1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	int* a;
+	int key, N, i, j;
+	srand(time(NULL));
+	setlocale(LC_ALL, "RU");
+	cout << "Введите колличество элементов =";
+	cin >> N;
+	a = new int[N];
+	for (i = 0; i < N; ++i)
+		a[i] = rand() % 99;
+
+	float start_time, end_time;
+
+	start_time = clock();
+	for (i = 1; i < N; i++)
+	{
+		key = a[i];
+		for (j = i - 1; j >= 0 && a[j] > key; j--)
+			a[j + 1] = a[j];
+
+		a[j + 1] = key;
+	}
+	end_time = clock();
+
+	cout << "Отсортированый массив: " << endl;
+	for (int i = 0; i < N; i++)
+		cout << a[i] << '\t';
+	cout << "" << endl;
+	cout << "Время сортировки,(мс)=";
+	cout << end_time - start_time << endl;
+}
